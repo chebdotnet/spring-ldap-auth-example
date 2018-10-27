b88149967d48ace608ef86d1904e709ebf299ad8
Aleksandrs Rihliks <aleksandrs.rihliks@gmail.com>
2018-10-27 21:23:21 +0300

    Initial commit

---
+++ src/main/java/alex/io/example/ldap/LdapApplication.java
@@ -0,0 +1,13 @@
+package alex.io.example.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapApplication.class, args);
+	}
+
+}
